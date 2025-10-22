13b73f6395dde73fb1b55ce2570cb704a25c1bed
Nzoowdev <nzoowcontact@gmail.com>
2025-10-22 07:26:53 +0000

    create script for except q and e

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -2,31 +2,22 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always à (Success)
  */
 int main(void)
 {
-char alphdep;
-char alphmid;
-char alphfin;
-alphdep = 'a';
-alphmid = 'f';
-alphfin = 'r';
-while (alphdep <= 'd')
-{
-putchar(alphdep);
-alphdep++;
-}
-while (alphmid <= 'p')
-{
-putchar(alphmid);
-alphmid++;
-}
-while (alphfin <= 'z')
-{
-putchar(alphfin);
-alphfin++;
-}
-putchar('\n');
-return (0);
+	char alph;
+
+	for (alph = 'a'; alph <= 'z' ; alph++)
+	{
+		if (alph == 'q' || alph == 'e')
+		{
+			continue;
+		}
+
+		putchar(alph);
+	}
+
+		putchar('\n');
+		return (0);
 }
