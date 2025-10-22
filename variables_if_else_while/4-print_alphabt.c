f335d1de70beb27b08d4c235f4f8d840e154e488
Nzoowdev <nzoowcontact@gmail.com>
2025-10-22 07:28:36 +0000

    create script for read alphabet but except e and q

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -6,18 +6,15 @@
  */
 int main(void)
 {
-	char alph;
-
-	for (alph = 'a'; alph <= 'z' ; alph++)
-	{
-		if (alph == 'q' || alph == 'e')
-		{
-			continue;
-		}
-
-		putchar(alph);
-	}
-
-		putchar('\n');
-		return (0);
+char alph;
+for (alph = 'a'; alph <= 'z' ; alph++)
+{
+if (alph == 'q' || alph == 'e')
+{
+continue;
+}
+putchar(alph);
+}
+putchar('\n');
+return (0);
 }
