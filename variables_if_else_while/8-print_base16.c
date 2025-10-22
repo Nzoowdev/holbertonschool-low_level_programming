6ea3575494675b87600f95b9c4f6c2d805cdb256
Nzoowdev <nzoowcontact@gmail.com>
2025-10-22 08:10:16 +0000

    create script for read number 0-9 and read aplhabet a-f

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+{char ch = '0';
+for (ch = '0'; ch <= '9'; ch++)
+putchar(ch);
+}
+{char alph = 'a';
+for (alph = 'a'; alph <= 'f'; alph++)
+putchar(alph);
+}
+putchar('\n');
+return (0);
+}
