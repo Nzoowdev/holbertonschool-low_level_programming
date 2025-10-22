d4e5cd8a7df354a6f6417891f8ca34d17ebb1e3b
Nzoowdev <nzoowcontact@gmail.com>
2025-10-22 14:33:21 +0000

    create script for print auto alphabet

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,15 +1,12 @@
 #include"main.h"
+#include<unistd.h>
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
+* print_alphabet - autoalphabet
 */
-int main(void)
+void print_alphabet(void)
 {
-    int alph = (print_alphabet);
-{
-    _putchar(alph);
-}
+char autoalph;
+for (autoalph = 'a'; autoalph <= 'z'; autoalph++)
+_putchar (autoalph);
 _putchar('\n');
-return(0);
 }
