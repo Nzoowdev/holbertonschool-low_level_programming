12a9162f5d85d01d5e2c5743c55bb25bf530e71c
Nzoowdev <nzoowcontact@gmail.com>
2025-10-22 13:29:13 +0000

    create script for draw alphabet with _putchar

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include"main.h"
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+    int alph = (print_alphabet);
+{
+    _putchar(alph);
+}
+_putchar('\n');
+return(0);
+}
