f8d5d52937a463c5386dd81c3dbd124c2e890781
Nzoowdev <nzoowcontact@gmail.com>
2025-10-22 07:54:18 +0000

    create script for read 0-9 with putchar

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always à (Success)
+ */
+int main(void)
+{
+char alph;
+for (alph = '0'; alph <= '9' ; alph++)
+{
+putchar(alph);
+}
+putchar('\n');
+return (0);
+}
