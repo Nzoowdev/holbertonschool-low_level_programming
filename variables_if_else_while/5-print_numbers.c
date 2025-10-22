33eea12c29d07f547f761fa28b7976fb8c10e018
Nzoowdev <nzoowcontact@gmail.com>
2025-10-22 07:38:53 +0000

    create script for exept 10 number

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always à (Success)
+ */
+int main(void)
+{
+int i = 0;
+do {
+printf("%d ", i);
+i++;
+}  while (i <= 9);
+return (0);
+}
