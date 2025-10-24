d06b5fc30c6cafa6ef1378898344cf6b3bb5c02f
Nzoowdev <nzoowcontact@gmail.com>
2025-10-24 08:00:04 +0000

    correction script for print with lower characher

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -2,20 +2,28 @@
 #include <unistd.h>
 #include <ctype.h>
 /**
- * _isalpha - checks for lowercase character
+ * _isalpha - checks for letter
  * @c: the character to check
  *
- * Return: 1 if c is lowercase, 0 otherwise
+ * Return: 1 if c is letter  lowercase or uppercase, 0 otherwise
  */
 
 int _isalpha(int c)
 
 {
-	if (c >= 'A' && c <= 'Z')
+	if (c >= 'a' && c <= 'z')
 	{
 		return (1);
 	}
+
+	else if (c >= 'A' && c <= 'Z')
+
+	{
+		return (1);
+	}
+
 	else
+
 	{
 		return (0);
 	}
