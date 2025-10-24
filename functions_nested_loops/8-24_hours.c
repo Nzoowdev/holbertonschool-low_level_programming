0497f193030af5a2ccd85590905a2d7022a90935
Nzoowdev <nzoowcontact@gmail.com>
2025-10-24 13:31:48 +0000

    create script for prints every minute

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+ int main (void)
+ {
+    int time = jack_bauer;
+    for (time = '00:00'; time <= '24:00'; time++)
+    
+    _putchar (time+ '00:01');
+    _putchar ('\n');
+    return(0);
+
+ }
