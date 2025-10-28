c7096ecfd3b663849c69fba98f4d2db89c768f9c
Nzoowdev <nzoowcontact@gmail.com>
2025-10-28 22:18:18 +0000

    create script for print \ if n is not egal of 0

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -2,7 +2,7 @@
 /**
  * print_diagonal - check the code
  *@n:valeur
- * 
+ *
  */
 void print_diagonal(int n)
 {
@@ -15,9 +15,11 @@ void print_diagonal(int n)
 			_putchar('\n');
 		}
 		else if (check <= 0)
-		_putchar('0');
-		_putchar(92);
-		_putchar('\n');
+		{
+			_putchar('0');
+			_putchar(92);
+			_putchar('\n');
+		}
 	}
 	_putchar('\n');
 }
