f625d84730d133f54bfe5f14f2db99f2745f276f
Nzoowdev <nzoowcontact@gmail.com>
2025-10-28 09:37:24 +0000

    fit: create script for print the number 0 to 9

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - check the code
+ *
+ * Return: Always 0.
+ */
+
+void print_numbers(void)
+{
+	int calc;
+
+	for (calc = '0'; calc <= '9'; calc++)
+	{
+		_putchar(calc);
+		_putchar('\n');
+	}
+	return (0);
+}
