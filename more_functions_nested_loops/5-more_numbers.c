fa8eea4444d16fdfe43839ceb9325e92a3aa1262
Nzoowdev <nzoowcontact@gmail.com>
2025-10-28 12:42:34 +0000

    create script for prints 10 ligne of 0 - 14

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,17 @@
+#include"main.h"
+/**
+* more_numbers - check the code
+*/
+void more_numbers(void)
+{
+int ligne;
+char nombre;
+{
+for (ligne = 0; ligne <= 10; ligne++)
+{
+for (nombre = '0'; nombre <= '14'; nombre++)
+_putchar (nombre);
+_putchar('\n');
+}
+}
+}
