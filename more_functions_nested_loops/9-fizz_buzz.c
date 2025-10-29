ed1bb99d90fed75ddd63baf2aec84bf64286b9ab
Nzoowdev <nzoowcontact@gmail.com>
2025-10-29 10:09:44 +0000

    create script for print multiples 3 and 5

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -6,23 +6,18 @@
 int main(void)
 {
 	int min1;
-	int min2;
-	int min3;
-	int resultat;
+	int min2 = '*3';
+	int min3 = '*5';
+	int resultat = min2 + min3;
 
 	for (min1 = 1; min1 <= 100; min1++)
 	{
-		for (min2 = 0; min2 <= 100; min2++)
-		{
-			if (min2 < 100)
-			_putchar("'*3' Fizz");
-		}
-		for (min3 = 0; min3 <= 100; min3++)
-		{
-			if (min3 < 100)
-			_putchar("'*5' Buzz");
-		}
+		if (min2 < '*3')
+			_putchar('Fizz');
+
+		if (min3 < '*5')
+			_putchar('Buzz');
 	}
-	
-	return(0);
+
+	return (0);
 }
