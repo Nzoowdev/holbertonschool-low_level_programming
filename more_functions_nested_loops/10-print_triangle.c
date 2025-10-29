17495eda0dd54d336f272d9eb51ab05f705d72d1
Nzoowdev <nzoowcontact@gmail.com>
2025-10-29 12:22:19 +0000

    create script for make triangle

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * main - Entry point for FizzBuzz.
+ *
+ * Return: Always 0.
+ */
+void print_triangle(int size)
+{
+	int space;
+
+	for (space = 0; space < size; space++)
+	{
+		if (size < 0)
+		{
+			_putchar(' ');
+		}
+		else if (size == 0)
+		{
+			_putchar('\n');
+		}
+		else 
+		{
+			_putchar('#');
+		}
+	_putchar('\n');
+	}
+}
