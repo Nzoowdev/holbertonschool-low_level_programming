9ac400a5281fe662ca327225ddfe42186e9a0c86
Nzoowdev <nzoowcontact@gmail.com>
2025-10-29 09:55:44 +0000

    create script for print multiples 3 and 5

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * print_square - check the code
+ *
+ */
+int main(void)
+{
+	int min1;
+	int min2;
+	int min3;
+	int resultat;
+
+	for (resultat = min1 + min2 + min3)
+	{
+		for (min1 = 1; min1 <= 100; min1++)
+		{
+			for (min2 = 0; min2 <= 100; min2++)
+			{
+				if (min2 < 100)
+					;
+				_putchar("'*3' Fizz");
+			}
+			for (min3 = 0; min3 <= 100; min3++)
+			{
+				if (min3 < 100)
+					;
+				_putchar("'*5' Buzz");
+			}
+		}
+	}
+	return (0);
+}
