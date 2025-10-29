2e39e5bcfee774835348f3adc9f2696af6f51df4
Nzoowdev <nzoowcontact@gmail.com>
2025-10-29 08:49:41 +0000

    create script for print cube

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -8,15 +8,15 @@ void print_square(int size)
 {
 	int longueur, largeur = 0;
 
+	if (size == 0)
+	{
+		_putchar('\n');
+	}
 	for (longueur = 0; longueur <= size; longueur++)
 	{
 		for (largeur = 0; largeur <= longueur; largeur++)
 		{
-			if (largeur == 0)
-			{
-				_putchar('\n');
-			}
-			else
+			if (largeur < size)
 			{
 				_putchar('#');
 			}
