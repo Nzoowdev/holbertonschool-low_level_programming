4f9e45f7a1bb02b9b9633f07d4632ba13fc57585
Nzoowdev <nzoowcontact@gmail.com>
2025-10-30 12:16:44 +0000

    feat: create script for change 2 value of pointer

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - check the code
+ * @a: pointer a
+ * @b: pointer b
+ */
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *b;
+	*a = *b;
+	*a = temp;
+}
