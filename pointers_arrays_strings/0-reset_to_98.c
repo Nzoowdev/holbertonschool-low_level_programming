355e16f34395482aed5e0939895e5be0460fe073
Nzoowdev <nzoowcontact@gmail.com>
2025-10-30 08:29:42 +0000

    fix: create script for make pointeur

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -8,14 +8,12 @@ void reset_to_98(int *n)
 {
 	int i;
 	int *p;
-	i = *p;
-	p = *n;
-	for (p = 0; p <= *n; n++)
+	i = &p;
+	p = n;
+
+	for (p = 0; p <= n; n++)
 	{
-		if (i = *n)
-		{
-			putchar(i);
-		}
-	putchar('\n');
+		putchar(i);
 	}
+	return;
 }
