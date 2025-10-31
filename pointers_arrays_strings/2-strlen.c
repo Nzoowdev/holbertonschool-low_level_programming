ef419cd27fab6196edcfa5c6c90d26031f9585a0
Nzoowdev <nzoowcontact@gmail.com>
2025-10-31 10:19:40 +0100

    fix:create script for print how much letter i have on word

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen - Returns the length of a string
+ * @s: Pointer to the string
+ *
+ * Return: Length of the string
+ */
+int _strlen(char *s)
+{
+	int e = 0;
+
+	for (e = 0; s[e] != '\0'; e++)
+    {
+		
+	}
+
+return (e);
+}
