fcab2b45e2e0693c73c0108ec726ec790441f320
Nzoowdev <nzoowcontact@gmail.com>
2025-11-01 12:36:41 +0000

    feat: create script for prints a string

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts - Returns the length of a string
+ * @str: Pointer to the string
+ *
+ * Return: Length of the string
+ */
+void _puts(char *str)
+{
+int e = 0;
+
+for (e = 0; str[e] != '\0'; e++)
+{
+}
+}
