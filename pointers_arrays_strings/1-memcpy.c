1bc9836c0232b0171799fbad4ea9bc5fa7f8b9eb
Nzoowdev <nzoowcontact@gmail.com>
2025-11-03 09:42:08 +0000

    feat: create script for copy memory

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _memset - function that fills memory with a constant byte
+ * @dest: table
+ * @src: variable
+ * @n: taille
+ * Return: always 0
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < src; i++)
+	{
+		if (i = src)
+		{
+			dest[i] = src;
+		}
+	}
+	return (dest);
+}
