0b73cd7752af40579ce6180e3b01dd2103a38162
Nzoowdev <nzoowcontact@gmail.com>
2025-11-04 12:53:54 +0000

    feat : create script for locates a substring

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strstr - function that copy memory with a constant byte
+ * @haystack: table
+ * @needle: variable
+ * Return: always 0
+ */
+char *_strstr(char *haystack, char *needle)
+{
+
+}
