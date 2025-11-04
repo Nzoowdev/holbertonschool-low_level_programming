483ba0ac99de974f6d2f03236d779fd6430fd82c
Nzoowdev <nzoowcontact@gmail.com>
2025-11-04 12:54:54 +0000

    fix : create script for locates a substring

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -8,5 +8,21 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
+	int i;
+	int j;
 
+	if (*needle == '\0')
+		return (haystack);
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i + j] != needle[j])
+				break;
+
+			if (needle[j + 1] == '\0')
+				return (haystack + i);
+		}
+	}
 }
