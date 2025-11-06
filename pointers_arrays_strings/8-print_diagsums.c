141327306df2faee3a0de4b6e562ed432f006719
Nzoowdev <nzoowcontact@gmail.com>
2025-11-06 09:13:06 +0000

    fix: create script for prints two diagonals

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -9,12 +9,13 @@
 void print_diagsums(int *a, int size);
 {
 	int i;
-	int j;
+	int sum1 = 0;
+	int sum2 = 0;
 
 	for (i = 0; i < size; i++)
 	{
-		i += *(a + i * size + i);
-		j += *(a + i * size + (size - 1 - i));
+		sum1 += *(a + i * size + i);
+		sum2 += *(a + i * size + (size - 1 - i));
 	}
 
 	printf("%d, %d\n", sum1, sum2);
