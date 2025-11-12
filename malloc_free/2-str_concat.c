60f9c5986e34e6a98c73efcc826eb167945e22f9
Nzoowdev <nzoowcontact@gmail.com>
2025-11-12 12:57:39 +0000

    fix : script for concatenates 2 strings

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,42 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
- * str_concat - function that concatenates two strings
+ * str_concat - concatenates two strings into a newly allocated space.
  *
  * @s1: the first string.
  * @s2: the second string.
  *
- * Return: returned pointer should point to a newly
+ * Return: pointer to the concatenated string, or NULL if it fails.
  */
+
 char *str_concat(char *s1, char *s2)
 {
 	char *concat;
-	int str1 = 0, i;
-	int str2 = 0, j;
+	int str1 = 0, str2 = 0, i, j;
 
 	if (s1 != NULL)
 	{
-		while(s1[str1] != '\0');
-		str1++;
+		while (s1[str1] != '\0')
+			str1++;
 	}
+
 	if (s2 != NULL)
 	{
-		while(s2[str2] != '\0');
-		str2++;
+		while (s2[str1] != '\0')
+			str2++;
 	}
+
 	concat = (char *)malloc((str1 + str2 + 1) * sizeof(char));
 	if (concat == NULL)
 	{
-		return(NULL);
+		return (NULL);
 	}
-	for (i = 0; i < str1;i++)
+
+	for (i = 0; i < str1; i++)
 	{
 		concat[i] = s1[i];
 	}
 
-	for (j = 0; j < str2;j++)
+	for (j = 0; j < str2; j++)
 	{
-		concat[j] = s2[j];
+		concat[i + j] = s2[j];
 	}
 	concat[str1 + str2] = '\0';
 
