8891ff88340091dd443c0259857eb51c75ac1438
Nzoowdev <nzoowcontact@gmail.com>
2025-11-12 09:10:53 +0000

    fix : create script for creates an array of chars, and initializes it with a specific char

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * create_array - creates an array of chars, and initializes it with a specific char
+ * @size: size of table
+ * @c: character on table
+ *
+ */
+char *create_array(unsigned int size, char c)
+{
+	int i;
+
+	for (i = 0; i < c; i++)
+	{
+		if (i == c)
+		{
+			i++;
+			break;
+		}
+	}
+}
