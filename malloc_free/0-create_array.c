40cfd4dd7fe4daf17b7daf6efde8be70bfd227eb
Nzoowdev <nzoowcontact@gmail.com>
2025-11-12 09:18:53 +0000

    fix : change description for over word in line

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * create_array - creates an array of chars, and initializes it with a specific char
+ * create_array - creates an array of chars, and initializes with specific char
  * @size: size of table
  * @c: character on table
  * Return: return pointer to array, or NULL if fails.
