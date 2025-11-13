d2a27cde159b1c456b2348eb915b630ccf9761d2
Nzoowdev <nzoowcontact@gmail.com>
2025-11-13 09:34:42 +0000

    fix: script for return pointers of 2 dimensionnal array

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -7,6 +7,7 @@
  * @width: the first string.
  * @height: the second string.
  *
+ * return - return code
  */
 
 int **alloc_grid(int width, int height)
@@ -22,4 +23,19 @@ int **alloc_grid(int width, int height)
 	if (grid == NULL)
 
 		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(sizeof(**grid) * width);
+
+		if (grid[i] == NULL)
+		{
+			free(grid);
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
+
+			grid[i][j] = 0;
+	}
+	return (grid);
 }
