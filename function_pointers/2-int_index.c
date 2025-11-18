8187ccfa755e978ccff73dc89a5db31178e453fe
Nzoowdev <nzoowcontact@gmail.com>
2025-11-18 09:11:50 +0000

    fix: create script for search integer in array

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -14,11 +14,11 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int i;
 
 	if (array == NULL || size <= 0 || cmp == NULL)
-	{
 		return (-1);
-	}
 	for (i = 0; i < size; i++)
 	{
-		return (-1);
+		if (cmp(array[i]) != 0)
+			return (i);
 	}
+	return (-1);
 }
