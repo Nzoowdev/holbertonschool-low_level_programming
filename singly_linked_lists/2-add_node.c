1b2e3f577300be30df7a0ae799ede6bcf35523ed
Nzoowdev <nzoowcontact@gmail.com>
2025-12-04 09:57:44 +0000

    feat: create script for adds a new node at the beginning of a list

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,47 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * add_node - adds a new node at the beginning of a list_t list
+ * @head: double pointer to the head of the list
+ * @str: string to be duplicated and added to the new node
+ *
+ * Return: the address of the new element, or NULL if it failed
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+    list_t *new_node;
+    char *dup_str;
+    unsigned int len = 0;
+
+    if (str == NULL)
+        return (NULL);
+
+    /* Duplicate the string */
+    dup_str = strdup(str);
+    if (dup_str == NULL)
+        return (NULL);
+
+    /* Calculate the length of the string */
+    while (str[len])
+        len++;
+
+    /* Allocate memory for the new node */
+    new_node = malloc(sizeof(list_t));
+    if (new_node == NULL)
+    {
+        free(dup_str);
+        return (NULL);
+    }
+
+    /* Initialize the new node */
+    new_node->str = dup_str;
+    new_node->len = len;
+    new_node->next = *head;
+
+    /* Update the head to point to the new node */
+    *head = new_node;
+
+    return (new_node);
+}
