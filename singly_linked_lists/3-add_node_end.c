da5d6dbc2408816df271f622f93cbd08d16486ee
Nzoowdev <nzoowcontact@gmail.com>
2025-12-04 10:40:41 +0000

    fix : betty code style

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -11,49 +11,49 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-    list_t *new_node, *temp;
-    char *dup_str;
-    unsigned int len = 0;
+	list_t *new_node, *temp;
+	char *dup_str;
+	unsigned int len = 0;
 
-    if (str == NULL)
-        return (NULL);
+	if (str == NULL)
+		return (NULL);
 
-    /* Duplicate the string */
-    dup_str = strdup(str);
-    if (dup_str == NULL)
-        return (NULL);
+	/* Duplicate the string */
+	dup_str = strdup(str);
+	if (dup_str == NULL)
+		return (NULL);
 
-    /* Calculate the length of the string */
-    while (str[len])
-        len++;
+	/* Calculate the length of the string */
+	while (str[len])
+		len++;
 
-    /* Allocate memory for the new node */
-    new_node = malloc(sizeof(list_t));
-    if (new_node == NULL)
-    {
-        free(dup_str);
-        return (NULL);
-    }
+	/* Allocate memory for the new node */
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+	{
+		free(dup_str);
+		return (NULL);
+	}
 
-    /* Initialize the new node */
-    new_node->str = dup_str;
-    new_node->len = len;
-    new_node->next = NULL;
+	/* Initialize the new node */
+	new_node->str = dup_str;
+	new_node->len = len;
+	new_node->next = NULL;
 
-    /* If the list is empty, make the new node the head */
-    if (*head == NULL)
-    {
-        *head = new_node;
-        return (new_node);
-    }
+	/* If the list is empty, make the new node the head */
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
 
-    /* Traverse to the end of the list */
-    temp = *head;
-    while (temp->next != NULL)
-        temp = temp->next;
+	/* Traverse to the end of the list */
+	temp = *head;
+	while (temp->next != NULL)
+		temp = temp->next;
 
-    /* Add the new node at the end */
-    temp->next = new_node;
+	/* Add the new node at the end */
+	temp->next = new_node;
 
-    return (new_node);
+	return (new_node);
 }
