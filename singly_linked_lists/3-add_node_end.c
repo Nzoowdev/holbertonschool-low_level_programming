0112c52573e646203839203442422b2fc2e81c3b
Nzoowdev <nzoowcontact@gmail.com>
2025-12-04 10:41:41 +0000

    fix : betty code style

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -18,16 +18,13 @@ list_t *add_node_end(list_t **head, const char *str)
 	if (str == NULL)
 		return (NULL);
 
-	/* Duplicate the string */
 	dup_str = strdup(str);
 	if (dup_str == NULL)
 		return (NULL);
 
-	/* Calculate the length of the string */
 	while (str[len])
 		len++;
 
-	/* Allocate memory for the new node */
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 	{
@@ -35,24 +32,19 @@ list_t *add_node_end(list_t **head, const char *str)
 		return (NULL);
 	}
 
-	/* Initialize the new node */
 	new_node->str = dup_str;
 	new_node->len = len;
 	new_node->next = NULL;
 
-	/* If the list is empty, make the new node the head */
 	if (*head == NULL)
 	{
 		*head = new_node;
 		return (new_node);
 	}
 
-	/* Traverse to the end of the list */
 	temp = *head;
 	while (temp->next != NULL)
 		temp = temp->next;
-
-	/* Add the new node at the end */
 	temp->next = new_node;
 
 	return (new_node);
