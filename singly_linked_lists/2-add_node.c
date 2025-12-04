8cb55e9381f27e2802af8730c7d5629469f810f8
Nzoowdev <nzoowcontact@gmail.com>
2025-12-04 10:02:07 +0000

    fix : betty code style

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -11,37 +11,37 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-    list_t *new_node;
-    char *dup_str;
-    unsigned int len = 0;
+	list_t *new_node;
+	char *dup_str;
+	unsigned int len = 0;
 
-    if (str == NULL)
-        return (NULL);
+	if (str == NULL)
+		return (NULL);
 
-    /* Duplicate the string */
-    dup_str = strdup(str);
-    if (dup_str == NULL)
-        return (NULL);
+	/* Duplicate the string */
+	dup_str = strdup(str);
+	if (dup_str == NULL)
+		return (NULL);
 
-    /* Calculate the length of the string */
-    while (str[len])
-        len++;
+	/* Calculate the length of the string */
+	while (str[len])
+		len++;
 
-    /* Allocate memory for the new node */
-    new_node = malloc(sizeof(list_t));
-    if (new_node == NULL)
-    {
-        free(dup_str);
-        return (NULL);
-    }
+	/* Allocate memory for the new node */
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+	{
+		free(dup_str);
+		return (NULL);
+	}
 
-    /* Initialize the new node */
-    new_node->str = dup_str;
-    new_node->len = len;
-    new_node->next = *head;
+	/* Initialize the new node */
+	new_node->str = dup_str;
+	new_node->len = len;
+	new_node->next = *head;
 
-    /* Update the head to point to the new node */
-    *head = new_node;
+	/* Update the head to point to the new node */
+	*head = new_node;
 
-    return (new_node);
+	return (new_node);
 }
