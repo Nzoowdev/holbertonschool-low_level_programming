5233befface0f03f099664dc5b1c6c210273dec9
Nzoowdev <nzoowcontact@gmail.com>
2025-12-06 13:10:59 +0000

    feat : create script prints a string, followed by a new line.

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
+
+/**
+ * _puts_recursion - prints a string, followed by a new line, using recursion
+ * @s: pointer to the string to print
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
