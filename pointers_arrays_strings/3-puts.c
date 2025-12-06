747726bc967a2b1768350bde2f1aef5865ac1197
Nzoowdev <nzoowcontact@gmail.com>
2025-12-06 12:39:54 +0000

    fix: script for prints a string, followed by a new line, to stdout

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,6 +1,15 @@
 #include "main.h"
 
 /**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
+
+/**
  * _puts - prints a string, followed by a new line, to stdout
  * @str: pointer to the string to print
  */
