ce1100acb2648ee70fdaa3de423e26b372bc9e5f
Nzoowdev <nzoowcontact@gmail.com>
2025-12-06 12:18:34 +0000

    feat: create main.h and add prototype

--- file_io/main.h
+++ file_io/main.h
@@ -1,14 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-size_t print_dlistint(const dlistint_t *h);
-size_t dlistint_len(const dlistint_t *h);
-dlistint_t *add_dnodeint(dlistint_t **head, const int n);
-dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
-void free_dlistint(dlistint_t *head);
-dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
-int sum_dlistint(dlistint_t *head);
-dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
-int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
+#include <stddef.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
 
 #endif
