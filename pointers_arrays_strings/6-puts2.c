0a522e7624a33a83d5c299f308bac85157c1efa7
Nzoowdev <nzoowcontact@gmail.com>
2025-12-06 12:44:51 +0000

    feat: create script for prints every other character of a string, starting with the first character, followed by a new line

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
+
+/**
+ * puts2 - prints every other character of a string, starting with the first
+ * @str: pointer to the string to print
+ */
+void puts2(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
