ccc6e24aa82373d620573eaf67d8dfd24fcd0e20
Nzoowdev <nzoowcontact@gmail.com>
2025-12-06 12:51:30 +0000

    feat : create script for convert a string to an integer.

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,55 @@
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: pointer to the string to convert
+ *
+ * Return: the integer value, or 0 if no numbers in string
+ */
+int _atoi(char *s)
+{
+	int i, sign, result, digit;
+	int started;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+	started = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-' && started == 0)
+		{
+			sign *= -1;
+		}
+		else if (s[i] == '+' && started == 0)
+		{
+			sign *= 1;
+		}
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			started = 1;
+			digit = s[i] - '0';
+			if (sign > 0)
+			{
+				result = result * 10 - digit;
+			}
+			else
+			{
+				result = result * 10 + digit;
+			}
+		}
+		else if (started == 1)
+		{
+			break;
+		}
+		i++;
+	}
+
+	if (sign > 0)
+	{
+		result = -result;
+	}
+
+	return (result);
+}
